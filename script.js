58786da04c099ab6c90523e49f24b5b52743180d
Vs-kishu <91000916+Vs-kishu@users.noreply.github.com>
2023-10-19 16:08:01 +0530

    added javascript to handle code output

---
+++ script.js
@@ -0,0 +1,31 @@
+const html_code = document.querySelector(".html-code textarea");
+const css_code = document.querySelector(".css-code textarea");
+const js_code = document.querySelector(".js-code textarea");
+const result = document.querySelector("#result");
+
+const styleType = document.querySelector("#style-type");
+styleType.addEventListener("change", run);
+function run() {
+  const styleType = document.querySelector("#style-type").value;
+  if (styleType === "css") {
+    result.contentDocument.body.innerHTML =
+      `<style>${css_code.value}</style>` + html_code.value;
+  } else if (styleType === "scss") {
+    // Compile SCSS to CSS using Sass.js
+    console.log("scss here");
+  }
+
+  result.contentWindow.eval(js_code.value);
+}
+
+// Checking if the user is typing anything in the input field
+html_code.onkeyup = () => run();
+css_code.onkeyup = () => run();
+js_code.onkeyup = () => run();
+
+// Accessing data stored in Local Storage. To make it more advanced, you could check if there is any data stored in Local Storage.
+html_code.value = localStorage.html_code;
+css_code.value = localStorage.css_code;
+js_code.value = localStorage.js_code;
+
+run();
